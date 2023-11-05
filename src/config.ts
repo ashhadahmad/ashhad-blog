1b3c40d8f64988011a1b352e40f670ed55906fdd
Ashhad Ahmad <ashhadahmed0@gmail.com>
2023-11-06 02:56:05 +0530

    add navigation to config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,8 @@
 // Header options
 export const siteTitle: string = "Ashhad";
 export const courseSite: string = "";
+export const links = [
+  { url: "/", label: "Home" },
+  { url: "/contact", label: "About" },
+  { url: "/about", label: "Contact" },
+];
