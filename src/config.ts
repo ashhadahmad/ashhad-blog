e291b6fcc9efc044a259a7a0fd18b9d743c1fbeb
Ashhad Ahmad <ashhadahmed0@gmail.com>
2023-11-06 02:58:04 +0530

    fixed about / contact url

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ export const siteTitle: string = "Ashhad";
 export const courseSite: string = "";
 export const links = [
   { url: "/", label: "Home" },
-  { url: "/contact", label: "About" },
-  { url: "/about", label: "Contact" },
+  { url: "/about", label: "About" },
+  { url: "/contact", label: "Contact" },
 ];
