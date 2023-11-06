60c31141f51b6cda1527d51438d8825f4a1b77e9
Ashhad Ahmad <ashhadahmed0@gmail.com>
2023-11-06 15:49:29 +0530

    add links page, add social link component

--- src/config.ts
+++ src/config.ts
@@ -5,11 +5,15 @@ export const links = [
   { url: "/", label: "Home" },
   { url: "/about", label: "About" },
   { url: "/contact", label: "Contact" },
+  { url: "/links", label: "Links" },
 ];
 export const socialLinks = {
   youtube: "https://www.youtube.com/@iamashhad",
+  youtube_devlabs: "https://www.youtube.com/@DevAshhad",
   twitter: "https://x.com/iam_ashhad",
   github: "https://github.com/ashhadahmad",
   instagram: "https://www.instagram.com/iamashhad/",
+  instagram_lifeofashhad: "https://www.instagram.com/lifeofashhad/",
   linkedin: "https://www.linkedin.com/in/ashhadahmad/",
+  topmate: "https://topmate.io/ashhad",
 };
